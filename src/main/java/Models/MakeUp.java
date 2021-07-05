31b3121ec0bd6ab4bd3016b3e9787541759554f9
Bobbi-Z <85585077+Bobbi-Z@users.noreply.github.com>
2021-07-05 12:56:19 -0400

    makeup properties added

--- src/main/java/Models/MakeUp.java
+++ src/main/java/Models/MakeUp.java
@@ -1,4 +1,11 @@
 package Models;
 
 public class MakeUp {
+    private int sku;
+    private String name;
+    private String brand;
+    private String type;
+    private String sectionOfFace;
+    private int qty;
+    private double price;
 }
