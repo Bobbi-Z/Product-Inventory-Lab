c693317937ecc912fa54b9c181a5afdec74d6b39
Bobbi-Z <85585077+Bobbi-Z@users.noreply.github.com>
2021-07-05 12:47:48 -0400

    hairproducts properties added

--- src/main/java/Models/HairProducts.java
+++ src/main/java/Models/HairProducts.java
@@ -1,4 +1,11 @@
 package Models;
 
 public class HairProducts {
+    private int sku;
+    private String name;
+    private String brand;
+    private String use;
+    private String typeOfHair;
+    private int qty;
+    private double price;
 }
