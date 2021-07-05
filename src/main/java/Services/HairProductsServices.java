5ad6bbf8707310b348b2cf4a897d3dc5290d0d4a
Bobbi-Z <85585077+Bobbi-Z@users.noreply.github.com>
2021-07-05 16:23:05 -0400

    hairproductsservices create added

--- src/main/java/Services/HairProductsServices.java
+++ src/main/java/Services/HairProductsServices.java
@@ -8,6 +8,17 @@ public class HairProductsServices{
 
     private static int nextSku = 1;
 
-    private ArrayList<HairProducts> inventory = new ArrayList<>();
+    private static ArrayList<HairProducts> inventory = new ArrayList<>();
 
+    public static HairProducts create(String name, String brand, String use,
+                                      String typeOfHair, int qty, double price) {
+
+        HairProducts createdHairProducts = new HairProducts(nextSku++, name, brand, use,
+                typeOfHair, qty, price);
+
+        inventory.add(createdHairProducts);
+
+        return createdHairProducts;
+
+    }
 }
