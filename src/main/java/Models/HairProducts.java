3d0aa661eb46c2abd998737957b8f0378e34dd4c
Bobbi-Z <85585077+Bobbi-Z@users.noreply.github.com>
2021-07-05 13:48:14 -0400

    hairproducts sku and name tests added

--- src/main/java/Models/HairProducts.java
+++ src/main/java/Models/HairProducts.java
@@ -8,4 +8,20 @@ public class HairProducts {
     private String typeOfHair;
     private int qty;
     private double price;
+
+    public void setSku(Integer sku) {
+    this.sku = sku;
+    }
+
+    public Integer getSku() {
+        return sku;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/test/java/Models/HairProductsTest.java
+++ src/test/java/Models/HairProductsTest.java
@@ -1,4 +1,30 @@
 package Models;
 
+import com.sun.tools.javac.util.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class HairProductsTest {
+
+    private HairProducts testHairProducts;
+
+    @Test
+    public void testSetSku(){
+        HairProducts hairProducts = new HairProducts();
+        Integer expected = 123456;
+        //given
+        hairProducts.setSku(expected);
+        Integer actual = hairProducts.getSku();
+        //when
+        Assertions.assertEquals(expected, actual);
+        //then
+    }
+    @Test
+    public void testSetName(){
+        HairProducts hairProducts = new HairProducts();
+        String expected = "Clean & Pure Nourishing Detox Shampoo";
+        hairProducts.setName(expected);
+        String actual = hairProducts.getName();
+        Assertions.assertEquals(expected, actual);
+    }
 }
