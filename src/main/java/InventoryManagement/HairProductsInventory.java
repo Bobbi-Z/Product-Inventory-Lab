ec008c5b851d5e9d6ef7370386b47b6c78b5fda6
Bobbi-Z <85585077+Bobbi-Z@users.noreply.github.com>
2021-07-06 22:51:18 -0400

    creattest

--- src/main/java/InventoryManagement/HairProductsInventory.java
+++ src/main/java/InventoryManagement/HairProductsInventory.java
@@ -1,11 +1,19 @@
 package InventoryManagement;
 
 import Models.HairProducts;
+import org.apache.commons.lang3.ArrayUtils;
 
 import java.util.ArrayList;
 
 public class HairProductsInventory {
     private static int nextSku = 1;
-    private ArrayList<HairProducts> inventory = new ArrayList<>();
+    private static ArrayList<HairProducts> inventory = new ArrayList<>();
 
+    public static HairProducts create(String name, String brand, String use,
+                                      String typeOfHair, int qty, double price){
+        HairProducts createdHairProducts = new HairProducts(nextSku++, name, brand,
+                use, typeOfHair, qty, price);
+        inventory.add(createdHairProducts);
+        return createdHairProducts;
+    }
 }
--- src/test/java/InventoryManagement/HairProductsInventoryTest.java
+++ src/test/java/InventoryManagement/HairProductsInventoryTest.java
@@ -1,8 +1,40 @@
 package InventoryManagement;
 
+import Models.HairProducts;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class HairProductsInventoryTest {
+
+    @Test
+    public void createTest(){
+        String expectedName = "Clean & Pure Nourishing Detox Shampoo";
+        String expectedBrand = "Nexxus";
+        String expectedUse = "Shampoo";
+        String expectedTypeOfHair = "1A, 1B, 1C, 2A, 2B, 2c";
+        int expectedQty = 100;
+        double expectedPrice = 13.99;
+
+        HairProductsInventory hairProductsInventory = new HairProductsInventory();
+        HairProducts testHairProducts = HairProductsInventory.create(expectedName,
+                expectedBrand,expectedUse,expectedTypeOfHair, expectedQty, expectedPrice);
+
+        int actualSku = testHairProducts.getSku();
+        String actualName = testHairProducts.getName();
+        String actualBrand = testHairProducts.getBrand();
+        String actualUse = testHairProducts.getUse();
+        String actualTypeOfHair = testHairProducts.getTypeOfHair();
+        int actualQty = testHairProducts.getQty();
+        double actualPrice = testHairProducts.getPrice();
+
+        Assertions.assertEquals(Integer.class.getName(),
+                new Integer(actualSku).getClass().getName());
+        Assertions.assertEquals(expectedName, actualName);
+        Assertions.assertEquals(expectedBrand, actualBrand);
+        Assertions.assertEquals(expectedUse, actualUse);
+        Assertions.assertEquals(expectedTypeOfHair, actualTypeOfHair);
+        Assertions.assertEquals(expectedQty, actualQty);
+        Assertions.assertEquals(expectedPrice, actualPrice);
+    }
 
 }
