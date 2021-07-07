938f05fd240e25478f0675d278d63803492aa6fc
Bobbi-Z <85585077+Bobbi-Z@users.noreply.github.com>
2021-07-06 22:23:49 -0400

    nextsku & product array list

--- src/main/java/InventoryManagement/HairProductsInventory.java
+++ src/main/java/InventoryManagement/HairProductsInventory.java
@@ -5,5 +5,7 @@ import Models.HairProducts;
 import java.util.ArrayList;
 
 public class HairProductsInventory {
+    private static int nextSku = 1;
+    private ArrayList<HairProducts> inventory = new ArrayList<>();
 
 }
