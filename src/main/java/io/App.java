186de9028e517216b51bb870abf7beba5651b71e
Bobbi-Z <85585077+Bobbi-Z@users.noreply.github.com>
2021-07-11 16:32:46 -0400

    committing choice 1 and 2

--- src/main/java/io/App.java
+++ src/main/java/io/App.java
@@ -3,6 +3,8 @@ package io;
 import Models.HairProducts;
 import Services.HairProductsServices;
 
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 //this will initialize the logic and services, and start the program
@@ -11,49 +13,87 @@ public class App {
     private HairProductsServices hairProductsServices = new HairProductsServices();
     //service needed to manage inventory
 
-    public static  void main(String...args){
+    public static void main(String... args) {
         App application = new App();
         //Instantiate the application
+        application.testInventory();
         application.init();
         //a method to initialize the application
     }
+    public void testInventory(){
+        hairProductsServices.create("Clean & Pure Nourishing Detox Shampoo", "Nexxus",
+                "Shampoo", "Straight", 10, 26.99);
+        hairProductsServices.create("Humectress Moisture Conditioner for Normal to Dry Hair",
+                "Nexxus", "Conditioner", "Straight", 10, 26.99);
+        hairProductsServices.create("Silk Express Miracle Silk Hair Mask", "It's A 10",
+                "Mask", "Straight", 10, 36.99);
+        hairProductsServices.create("Ghost Weightless Hair Oil", "Verb", "Style",
+                "Straight", 10, 18.00);
 
-    public void init(){
+    }
+
+    public void init() {
         Scanner input = new Scanner(System.in);
-        boolean mainMenu = true;
         int choice = 0;
-        do{ Console.printWelcome();
-            Console.mainMenu();
-            System.out.println("Please enter your choice: ");
-            choice = input.nextInt();
-        }
-        while (choice < 7);
+        Console.printWelcome();
+        System.out.println("Press any number to go to the main menu");
+        choice = input.nextInt();
+        mainMenu();
+    }
+     public void mainMenu(){
+        Scanner input = new Scanner(System.in);
+         int choice = 0;
+        Console.mainMenu();
+        System.out.println("Please enter your choice: ");
+        //input.nextLine();
+        choice = input.nextInt();
 
         switch (choice) {
-
             case 1:
-                hairProductsServices.findAll();
-                if (hairProductsServices.inventory.isEmpty()) {
-                    System.out.println("There is no current inventory");
-                } else {
-                    System.out.println(hairProductsServices.inventory);
-                }
-                break;
+                choiceOne();
 
-            case 2:
+        }
+    }
+    public void choiceOne(){
+            Scanner scanOne = new Scanner(System.in);
+            ArrayList<HairProducts> allHairProducts = hairProductsServices.findAll();
+            if (hairProductsServices.inventory.isEmpty()) {
+                System.out.println("There is no current inventory");
+            } else {
+                for (HairProducts element : allHairProducts) {
+                    System.out.println("SKU: " + element.getSku() + " Name: " + element.getName() +
+                            " Brand: " + element.getBrand() + " Use: " + element.getUse() +
+                            " Hair Type: " + element.getTypeOfHair() + " Quantity: " + element.getQty() +
+                            " Price: " + element.getPrice());
+                }
+
+            }
+        System.out.println("Press any button to return to the main menu");
+            scanOne.nextLine();
+            mainMenu();
+        }
+    public void choiceTwo(){
+        Scanner scanTwo = new Scanner(System.in);
                 int sku = 0;
-                System.out.println("Please enter sku: ");
-                sku = input.nextInt();
+         Console.pickHairOrMake();
+        System.out.println("Please choose ");
+        int choiceHM = scanTwo.nextInt();
+        if(choiceHM == 1) { //choosing hair
+            System.out.println("Please enter sku: ");
+            scanTwo.nextLine();
+            sku = scanTwo.nextInt();
+            HairProducts found = hairProductsServices.findHairProduct(sku);
+            if (found == null) {
+                System.out.println("This is not a valid sku");
+            } else {
+                System.out.println(found);
+            }
+        }
+    }
 
-                hairProductsServices.findHairProduct(sku);
-                if (hairProductsServices.findHairProduct(sku) == null) {
-                    System.out.println("This is not a valid sku");
-                } else {
-                    System.out.println(hairProductsServices.inventory.get(sku));
-                }
-                break;
 
-            case 3:
+
+         /*   case 3:
                 String name = "";
                 String brand = "";
                 int useChoice = 0;
@@ -121,21 +161,26 @@ public class App {
                         hairProductsServices.inventory.get(hairProductsServices.inventory.size()));
                 break;
 
-            case 4: //Should be update product - couldn't figure it out
+            case 4:
+                Console.changeQtyOrPrice();
+                input.nextLine();
                 System.out.println("Please enter sku: ");
                 sku = input.nextInt();
 
-                hairProductsServices.delete(sku);
-                if (hairProductsServices.findHairProduct(sku) == null) {
-                    System.out.println("This is not a valid sku");
-                } else {
-                    System.out.println(sku + " has been deleted.");
-                }
-                break;
-
-       //   case 5:
 
             case 5:
+                 System.out.println("Please enter sku: ");
+                 sku = input.nextInt();
+
+                 hairProductsServices.delete(sku);
+                 if (hairProductsServices.findHairProduct(sku) == null) {
+                     System.out.println("This is not a valid sku");
+                 } else {
+                     System.out.println(sku + " has been deleted.");
+                 }
+                 break;
+
+            case 6:
                 System.out.println("Returning to Welcome Screen");
                 System.exit(6);
 
@@ -145,6 +190,6 @@ public class App {
         }
 
 
-    }
+    }*/
 
-}
+    }
--- src/main/java/io/Console.java
+++ src/main/java/io/Console.java
@@ -3,50 +3,72 @@ package io;
 public class Console {
     public static void printWelcome(){
         System.out.println(""+
-                "******************************************************" +
-                "***              Welcome and Bienvenue             ***" +
-                "***                       to                       ***" +
-                "***               B's Inventory Manager            ***" +
-                "******************************************************");
+                "******************************************************\n" +
+                "***              Welcome and Bienvenue             ***\n" +
+                "***                       to                       ***\n" +
+                "***               B's Inventory Manager            ***\n" +
+                "******************************************************\n");
     }
     public static void mainMenu(){
         System.out.println(""+
-                "******************************************************" +
-                "***                    Main Menu                   ***" +
-                "***                                                ***" +
-                "***    1. Get complete inventory sorted by sku     ***" +
-                "***    2. Search for a specific product by sku     ***" +
-                "***    3. Create new product                       ***" +
-                "***    4. Update existing product                  ***" +
-                "***    5. Delete product                           ***" +
-                "***    6. Exit                                     ***" +
-                "******************************************************");
+                "******************************************************\n" +
+                "***                    Main Menu                   ***\n" +
+                "***                                                ***\n" +
+                "***    1. Get complete inventory sorted by sku     ***\n" +
+                "***    2. Search for a specific product by sku     ***\n" +
+                "***    3. Create new product                       ***\n" +
+                "***    4. Update existing product                  ***\n" +
+                "***    5. Delete product                           ***\n" +
+                "***    6. Exit                                     ***\n" +
+                "******************************************************\n");
     }
 
     public static void productUse(){
         System.out.println(""+
-                "******************************************************" +
-                "***               Product Use Choices              ***" +
-                "***                                                ***" +
-                "***    1. Shampoo                                  ***" +
-                "***    2. Conditioner                              ***" +
-                "***    3. Mask                                     ***" +
-                "***    4. Style                                    ***" +
-                "***    5. Exit                                     ***" +
-                "******************************************************");
+                "******************************************************\n" +
+                "***               Product Use Choices              ***\n" +
+                "***                                                ***\n" +
+                "***    1. Shampoo                                  ***\n" +
+                "***    2. Conditioner                              ***\n" +
+                "***    3. Mask                                     ***\n" +
+                "***    4. Style                                    ***\n" +
+                "***    5. Exit                                     ***\n" +
+                "******************************************************\n");
 
     }
 
     public static void hairType() {
         System.out.println(""+
-                "******************************************************" +
-                "***                Hair Type Choices               ***" +
-                "***                                                ***" +
-                "***    1. Straight | 1a-1c                         ***" +
-                "***    2. Wavy     | 2a-2c                         ***" +
-                "***    3. Curly    | 3a-3c                         ***" +
-                "***    4. Coily    | 4a-4c                         ***" +
-                "***    5. Exit                                     ***" +
-                "******************************************************");
+                "******************************************************\n" +
+                "***                Hair Type Choices               ***\n" +
+                "***                                                ***\n" +
+                "***    1. Straight | 1a-1c                         ***\n" +
+                "***    2. Wavy     | 2a-2c                         ***\n" +
+                "***    3. Curly    | 3a-3c                         ***\n" +
+                "***    4. Coily    | 4a-4c                         ***\n" +
+                "***    5. Exit                                     ***\n" +
+                "******************************************************\n");
     }
+
+    public static void changeQtyOrPrice(){
+        System.out.println(""+
+                "******************************************************\n" +
+                "***   Do you want to change the price or quantity  ***\n" +
+                "***                                                ***\n" +
+                "***    1. Quantity                                 ***\n" +
+                "***    2. Price                                    ***\n" +
+                "***    3. Exit                                     ***\n" +
+                "******************************************************\n");
+    }
+    public static void pickHairOrMake(){
+        System.out.println(""+
+                "******************************************************\n" +
+                "***      Do you want to search Hair or MakeUp?     ***\n" +
+                "***                                                ***\n" +
+                "***    1. Hair Products                            ***\n" +
+                "***    2. MakeUp Products                          ***\n" +
+                "***    3. Exit                                     ***\n" +
+                "******************************************************\n");
+    }
+
 }
